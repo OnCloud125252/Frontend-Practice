7a87bf69e76440a2a068f3d2c9c5dbd16f83067d
OnCloud <anonymousaaaa41414141@gmail.com>
2023-03-11 21:29:25 +0800

    Display selection on screen
    Fix
    -When the search bar is empty, it'll display "No Result"

--- Debounce/index.js
+++ Debounce/index.js
@@ -1,26 +1,79 @@
-const api_host = "https://640ad5ee81d8a32198d1c6ae.mockapi.io";
+const api_url = "https://640ad5ee81d8a32198d1c6ae.mockapi.io/api/products";
 
 
-const input = document.getElementById("searchBox");
-var timeout;
-input.addEventListener("input", (rawInput) => {
-    const input = rawInput.target.value;
-    clearTimeout(timeout);
-    if (input === "") return;
-    timeout = setTimeout(async () => {
-        var productsList = await api("products");
-        productsList = productsList
-            .map(product => product.name)
-            .filter(name => name.toLowerCase().includes(input))
-            .sort((a, b) => a.length - b.length)
-            .sort((a, b) => a.toLowerCase().indexOf(input) - b.toLowerCase().indexOf(input));
+var settings = {
+    placeholder: "Search For Products",
+    lengthLimit: 5,
+    timeoutMs: 500
+};
 
-        console.log(productsList.join("\n"));
-    }, 800);
+const input = document.getElementById("inputBox");
+const productsSelections = document.getElementById("productsSelections");
+var timeoutVariable;
+
+window.onload = () => {
+    input.focus();
+    input.setAttribute("placeholder", "Search For Products");
+};
+
+input.addEventListener("focus", () => {
+    searchProducts(input.value);
+    return productsSelections.style = "";
+});
+input.addEventListener("blur", () => {
+    setTimeout(() => {
+        return productsSelections.style = "display: none;";
+    }, 150);
 });
 
-async function api(route) {
-    const response = await fetch(api_host + "/api/" + route, {
+input.addEventListener("input", (rawInput) => {
+    const inputValue = rawInput.target.value;
+
+    searchProducts(inputValue);
+});
+
+
+function searchProducts(inputValue) {
+    clearTimeout(timeoutVariable);
+
+    const lengthLimit = settings.lengthLimit ?? 10;
+    const timeoutMs = settings.timeoutMs ?? 500;
+
+    if (input.value.length === 0) {
+        console.log("Search bar is empty");
+        return productsSelections.innerHTML = "";
+    };
+
+    timeoutVariable = setTimeout(async () => {
+        console.log("User search for: " + inputValue);
+        var productsList = await api(api_url);
+        productsList = productsList
+            .map(product => product.name)
+            .filter(name => name.toLowerCase().includes(inputValue.toLowerCase()))
+            .sort((a, b) => a.length - b.length)
+            .sort((a, b) => a.toLowerCase().indexOf(inputValue.toLowerCase()) - b.toLowerCase().indexOf(inputValue.toLowerCase()));
+
+        productsList = productsList.map(productName => `<li class="selection allowed" onclick="setInput(this.textContent)">${productName}</li>`);
+
+        if (input.value.length !== 0 && productsList.length === 0) {
+            console.log("No search result");
+            productsSelections.innerHTML = `<li class="selection not_allowed">No Result</li>`;
+        }
+        else {
+            productsList.length = lengthLimit;
+            productsSelections.innerHTML = productsList.join("");
+        };
+    }, timeoutMs);
+}
+
+function setInput(text) {
+    input.value = text;
+    input.focus();
+    return productsSelections.innerHTML = "";
+}
+
+async function api(url) {
+    const response = await fetch(url, {
         method: 'GET',
         headers: { 'content-type': 'application/json' },
     });
