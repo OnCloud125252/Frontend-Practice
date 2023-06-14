4aecfbc2564d9725de2e76f30c87a2f1f24a9bbf
Alex Liao <anonymousaaaa41414141@gmail.com>
2023-06-14 15:37:38 +0800

    Add settings field for pure-css-button

--- pages/pure-css-button/static/index.js
+++ pages/pure-css-button/static/index.js
@@ -1,11 +1,32 @@
+var pure_css_buttonSettings = {
+    lightTheme: false,
+    hoverLayer: false,
+    hoverBox: false
+}
+
 const background = document.getElementById("background");
-let lightTheme = false;
+const hoverLayerInput = document.getElementById("hoverLayer");
+const hoverLayer = document.getElementsByClassName("hoverLayer");
+const hoverBoxInput = document.getElementById("hoverBox");
+const hoverBoxes = document.getElementsByName("hoverBox");
+
+hoverLayerInput.addEventListener("change", (input) => {
+    pure_css_buttonSettings.hoverLayer = input.target.checked;
+    pure_css_buttonSettings.hoverLayer ? hoverLayer[0].classList.add("showHoverLayer") : hoverLayer[0].classList.remove("showHoverLayer");
+})
+
+hoverBoxInput.addEventListener("change", (input) => {
+    pure_css_buttonSettings.hoverBox = input.target.checked;
+    hoverBoxes.forEach(box => {
+        pure_css_buttonSettings.hoverBox ? box.classList.add("showHoverBox") : box.classList.remove("showHoverBox");
+    });
+});
 
 function toggleTheme() {
-    if (lightTheme) {
+    if (pure_css_buttonSettings.lightTheme) {
         background.classList.replace("light", "dark");
-    } else if (!lightTheme) {
+    } else if (!pure_css_buttonSettings.lightTheme) {
         background.classList.replace("dark", "light");
     }
-    lightTheme = !lightTheme;
+    pure_css_buttonSettings.lightTheme = !pure_css_buttonSettings.lightTheme;
 }
